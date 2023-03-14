3256fe255f3865346bb195ef34b5f732bde177a0
mahbub045 <mahbubrmn45@gmail.com>
2023-03-14 11:51:43 +0600

    initial Upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+const MONGO_URL = "mongodb+srv://mahbub045:G71rEXEZ47BCoN6G@cluster0.ed6fkc3.mongodb.net/friendbuzz?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URL).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Error connecting to MongoDB:', err);
+});
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+    console.log("Backend server is running!");
+});
---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const User = require("../models/User");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    try {
+        //generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        //create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+            desc: req.body.desc
+        });
+
+        //save user and return response
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+    };
+});
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).json("User not found!");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("Wrong Password!");
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
